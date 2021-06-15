9d4aecc5db86b801f8426df6cc566bbce27c2dd3
Alexandr Dmitriev <alexandrvdmitriev@gmai.com>
2021-06-15 15:43:08 +0300

    Minimal working JSON load from schema

--- frontend/pyQT/pyQT_client.py
+++ frontend/pyQT/pyQT_client.py
@@ -1,5 +1,13 @@
-from PyQt5 import QtWidgets, uic
+from PyQt5 import QtWidgets, uic, QtCore
+from PyQt5.QtWidgets import QApplication, QWidget, QInputDialog, QLineEdit, QFileDialog
+from PyQt5.QtGui import QIcon
+
 import sys
+import json
+import argparse
+import collections
+from json import dumps
+
 from qt_jsonschema_form import WidgetBuilder
 
 class Ui(QtWidgets.QMainWindow):
@@ -15,6 +23,31 @@ class Ui(QtWidgets.QMainWindow):
     def printButtonPressed(self):
         # This is executed when the button is pressed
         print('printButtonPressed')
+        print(type(100))
+        a = 3
+        options = QFileDialog.Options()
+        options |= QFileDialog.DontUseNativeDialog
+        fileName, _ = QFileDialog.getOpenFileName(self,"Load JSON scheme file", "","All Files (*);;JSON schema files (*.json)", options=options)
+        if fileName:
+            self.jsonschemafile = self.findChild(QtWidgets.QPlainTextEdit, 'jsonschemafile') # Find the button
+            self.jsonschemafile.setPlainText(fileName)
+            print(fileName)
+        jfile = open(fileName)
+        schema = json.load(jfile, object_pairs_hook=collections.OrderedDict)
+        builder = WidgetBuilder()
+        ui_schema = {
+            "schema_path": {
+                "ui:widget": "filepath"
+            },
+            "buil_color": {
+                "ui:widget": "colour"
+            }
+
+        }
+        form = builder.create_form(schema, ui_schema)
+        form.show()
+        form.widget.on_changed.connect(lambda d: print(dumps(d, indent=4)))
+
 
 app = QtWidgets.QApplication(sys.argv)
 window = Ui()
