3bc0225ad180dd15778c27cde29f6a005734103d
Alexandr Dmitriev <alexandrvdmitriev@gmai.com>
2021-06-16 17:19:08 +0300

    Feature added: show current JSON as tree

--- frontend/pyQT/pyQT_client.py
+++ frontend/pyQT/pyQT_client.py
@@ -10,7 +10,29 @@ from json import dumps
 
 from qt_jsonschema_form import WidgetBuilder
 
+class TextToTreeItem:
+
+    def __init__(self):
+        self.text_list = []
+        self.titem_list = []
+
+    def append(self, text_list, titem):
+        for text in text_list:
+            self.text_list.append(text)
+            self.titem_list.append(titem)
+
+    # Return model indices that match string
+    def find(self, find_str):
+
+        titem_list = []
+        for i, s in enumerate(self.text_list):
+            if find_str in s:
+                titem_list.append(self.titem_list[i])
+
+        return titem_list
+
 class Ui(QtWidgets.QMainWindow):
+
     def __init__(self):
         super(Ui, self).__init__()
         uic.loadUi('pyQT_client.ui', self)
@@ -24,11 +46,56 @@ class Ui(QtWidgets.QMainWindow):
         self.buttonPOST = self.findChild(QtWidgets.QPushButton, 'postJSON')
         self.buttonPOST.clicked.connect(self.postJSONPressed)
 
+        self.buttonShowJSON = self.findChild(QtWidgets.QPushButton, 'showJSON')
+        self.buttonShowJSON.clicked.connect(self.ShowJSONPressed)
+
         self.show()
 
     def on_json_data_change(self,d):
         print(d)
         self.json_data = d
+
+    def ShowJSONPressed(self):
+        self.ShowJSON(self.json_data)
+
+    def ShowJSON(self, json_data_input):
+        self.text_to_titem = TextToTreeItem()
+        self.tree_widget= self.findChild(QtWidgets.QTreeWidget, 'JSONtreeWidget')
+        self.tree_widget.setHeaderLabels(["Key", "Value"])
+        self.tree_widget.header().setSectionResizeMode(QtWidgets.QHeaderView.Stretch)
+        root_item = QtWidgets.QTreeWidgetItem(["Root"])
+        self.recurse_jdata(json_data_input, root_item)
+        self.tree_widget.addTopLevelItem(root_item)
+        self.tree_widget.show()
+
+
+
+    def recurse_jdata(self, jdata, tree_widget):
+        if isinstance(jdata, dict):
+            for key, val in jdata.items():
+                self.tree_add_row(key, val, tree_widget)
+        elif isinstance(jdata, list):
+            for i, val in enumerate(jdata):
+                key = str(i)
+                self.tree_add_row(key, val, tree_widget)
+        else:
+            print("This should never be reached!")
+
+    def tree_add_row(self, key, val, tree_widget):
+        text_list = []
+
+        if isinstance(val, dict) or isinstance(val, list):
+            text_list.append(key)
+            row_item = QtWidgets.QTreeWidgetItem([key])
+            self.recurse_jdata(val, row_item)
+        else:
+            text_list.append(key)
+            text_list.append(str(val))
+            row_item = QtWidgets.QTreeWidgetItem([key, str(val)])
+
+        tree_widget.addChild(row_item)
+        self.text_to_titem.append(text_list, row_item)
+
 
     def selectSchemaButtonPressed(self):
         options = QFileDialog.Options()
