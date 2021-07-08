3908e75258ec0c9c9fe05bbc4429b4cd22f07935
Alexandr Dmitriev <alexandrvdmitriev@gmai.com>
2021-07-08 15:35:24 +0300

    Doc: update on database install

--- install_db/populate_db.sh
+++ install_db/populate_db.sh
@@ -3,4 +3,5 @@ curl --user admin:secret -I -X PUT localhost:8080/
 curl --user admin:secret -I -X PUT localhost:8080/_schemas
 curl --user admin:secret -I -X PUT localhost:8080/_schemas/inventory -T "basic_schema.json"  -H "Content-Type: application/json"
 curl --user admin:secret -I -X PUT localhost:8080/inventory
+curl --user admin:secret -I -X PUT localhost:8080/inventory.files
 curl --user admin:secret -I -X PUT localhost:8080/inventory -T "inventory_meta.json"  -H "Content-Type: application/json"
